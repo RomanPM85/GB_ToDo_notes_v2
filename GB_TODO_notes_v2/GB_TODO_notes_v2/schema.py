84e4df28afc0d949dafcff614975e01f563e2813
RomanPM85 <mamchiy@yandex.ru>
2022-11-11 01:15:13 +0300

    using filtering to users and projects

--- GB_TODO_notes_v2/GB_TODO_notes_v2/schema.py
+++ GB_TODO_notes_v2/GB_TODO_notes_v2/schema.py
@@ -37,5 +37,20 @@ class Query(ObjectType):
     def resolve_all_todo(root, info):
         return TODO.objects.all()
 
+    # all_users_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
+    all_users_by_id = graphene.List(UserType, id=graphene.Int(required=False))
+
+    def resolve_all_users_by_id(root, info, id=None):
+        if id:
+            return User.objects.get(id=id)
+        return User.objects.all()
+
+    project_by_users = graphene.List(ProjectType, last_name=graphene.String(required=False))
+
+    def resolve_project_by_users(root, info, last_name=None):
+        if last_name:
+            return Project.objects.filter(users__last_name=last_name)
+        return Project.objects.all()
+
 
 schema = graphene.Schema(query=Query)
