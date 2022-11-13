39cb43e46023a39bed31898f2af4d72b260a6bde
RomanPM85 <mamchiy@yandex.ru>
2022-11-14 00:48:02 +0300

    UserUpdateMutation

--- GB_TODO_notes_v2/GB_TODO_notes_v2/schema.py
+++ GB_TODO_notes_v2/GB_TODO_notes_v2/schema.py
@@ -23,65 +23,64 @@ class TODOType(DjangoObjectType):
         fields = '__all__'
 
 
-# class Query(ObjectType):
-    # all_users = graphene.List(UserType)
-    # all_project = graphene.List(ProjectType)
-    # all_todo = graphene.List(TODOType)
+class Query(ObjectType):
+    all_users = graphene.List(UserType)
+    all_project = graphene.List(ProjectType)
+    all_todo = graphene.List(TODOType)
 
-    # def resolve_all_users(root, info):
-    #     return User.objects.all()
-    #
-    # def resolve_all_project(root, info):
-    #     return Project.objects.all()
-    #
-    # def resolve_all_todo(root, info):
-    #     return TODO.objects.all()
+    def resolve_all_users(root, info):
+        return User.objects.all()
 
-    # all_users_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
+    def resolve_all_project(root, info):
+        return Project.objects.all()
+
+    def resolve_all_todo(root, info):
+        return TODO.objects.all()
+
+    users_by_id = graphene.Field(UserType, id=graphene.Int(required=True))
     # all_users_by_id = graphene.List(UserType, id=graphene.Int(required=False))
 
-    # def resolve_all_users_by_id(root, info, id=None):
-    #     if id:
-    #         return User.objects.get(id=id)
-    #     return User.objects.all()
-    #
-    # project_by_users = graphene.List(ProjectType, last_name=graphene.String(required=False))
-    #
-    # def resolve_project_by_users(root, info, last_name=None):
-    #     if last_name:
-    #         return Project.objects.filter(users__last_name=last_name)
-    #     return Project.objects.all()
+    def resolve_users_by_id(self, info, id):
+        try:
+            return User.objects.get(id=id)
+        except User.DoesNotExist:
+            return None
+
+    project_by_users = graphene.List(ProjectType, last_name=graphene.String(required=False))
+
+    def resolve_project_by_users(root, info, last_name=None):
+        if last_name:
+            return Project.objects.filter(users__last_name=last_name)
+        return Project.objects.all()
 
 
 # class UserUpdateMutation(graphene.Mutation):
 #     pass
 
 
-class UserCreateMutation(graphene.Mutation):
+class UserUpdateMutation(graphene.Mutation):
     class Arguments:
         user_name = graphene.String()
-        first_name = graphene.String()
-        last_name = graphene.String()
-        email = graphene.String()
+#         first_name = graphene.String()
+#         last_name = graphene.String()
+#         email = graphene.String()
+        id = graphene.ID()
 
     user = graphene.Field(UserType)
 
     @classmethod
-    def mutate(cls, root, info, user_name, first_name, last_nam, email):
-        user = User(user_name=user_name, first_name=first_name, last_nam=last_nam, email=email)
+    def mutate(cls, root, info, user_name, id):
+        user = User.objects.get(pk=id)
+        user.user_name = user_name
         user.save()
-        return UserCreateMutation(user=user)
-
-
-# class UserDeleteMutation(graphene.Mutation):
-#     pass
+        return UserUpdateMutation(user=user)
 
 
 class Mutations(ObjectType):
-    # update_user = UserUpdateMutation.Field()
-    create_user = UserCreateMutation.Field()
+    update_user = UserUpdateMutation.Field()
+    # create_user = UserCreateMutation.Field()
     # delete_user = UserDeleteMutation.Field()
 
 
-# schema = graphene.Schema(query=Query)
-schema = graphene.Schema(mutation=Mutations)
+schema = graphene.Schema(query=Query, mutation=Mutations)
+# schema = graphene.Schema(mutation=Mutations)
