5d3658b55569825c9a4e91db0b01bcbca8dd4759
RomanPM85 <mamchiy@yandex.ru>
2022-12-11 01:29:51 +0300

    create dockerfile, docker-compose, and run adminer

---
+++ frontend/venv_project.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+deactivate
+echo "deactivate Ok"
+cd
+cd GeekBrains/ToDoNotes/
+source venv/bin/activate
+echo "venv/bin/activate Ok"
+cd GB_ToDo_notes_v2/GB_TODO_notes_v2/frontend/
+echo "Hello Roman! this is dir frontend project venv"
