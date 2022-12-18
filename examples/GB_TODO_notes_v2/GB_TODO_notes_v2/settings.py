0df98b721607deb974fe28e0a477b9617320b75c
RomanPM85 <mamchiy@yandex.ru>
2022-12-19 01:20:17 +0300

    create Dockerfile and wait-for-postgres.sh

--- examples/GB_TODO_notes_v2/GB_TODO_notes_v2/settings.py
+++ examples/GB_TODO_notes_v2/GB_TODO_notes_v2/settings.py
@@ -86,10 +86,21 @@ WSGI_APPLICATION = 'GB_TODO_notes_v2.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'todo',
+        'USER': 'dante',
+        'PASSWORD': 'dante123456',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
